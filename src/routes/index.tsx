cb6877af55923eaa242ab7d6548780f292cfcbce
Bryceson Laing <bklaing2@gmail.com>
2026-05-27 09:18:13 -0500

    utilize song update logic in `<Song>`

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { Song } from '#/components/song.tsrx';
 import { getSong } from '#/fs'
 import type { Song as SongType } from '#/types';
 import { createFileRoute } from '@tanstack/solid-router'
-import { createSignal } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
 
 export const Route = createFileRoute('/')({ component: Home })
 
@@ -10,10 +10,10 @@ function Home() {
   const [song, setSong] = createSignal<SongType | null>(null);
 
   return (
-    <div>
+    <>
       <button onClick={() => getSong().then(setSong)}>Select song</button>
 
-      <Song song={song()} />
-    </div>
+      <Show when={song()}>{(song) => <Song song={song()} setSong={setSong} />}</Show>
+    </>
   )
 }
